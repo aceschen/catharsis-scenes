aeb76d6d7d238750705a3e8fd893f72b197ee6f6
Ace Chen <12aschen@berkeley.edu>
2024-11-30 10:27:41 -0800

    little fixes

--- sketches/choices.js
+++ sketches/choices.js
@@ -1,34 +1,28 @@
 let sceneText = [];
-let scene = [];
-let scenePrinter = [];
 
 let lineNumber = 0; 
 
-let wordCount = 0;
 let charPrint = 1;
 let currentText = "";
 
 let inBrackets = false;
 let choices = ["", ""];
-let lastChoiceIndex = 0;
-let pauseForInput = false;
 
 let pause = 0;
+let pauseForInput = false;
+
 let wHeight;
 let scrollOffset = 120;
 
 let jetbrains;
 
 function preload() {
-  sceneText = loadStrings('/assets/scene4.txt');
+  sceneText = loadStrings('/assets/choices.txt');
   jetbrains = loadFont('/assets/JetBrainsMono-VariableFont_wght.ttf');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  scenePrinter.push(sceneText[0]);
-  scenePrinter.push(sceneText[1]);
-  wordCount = scenePrinter[0].length;
+  createCanvas(windowWidth - 20, windowHeight - 20);
   frameRate(48);
   wHeight = windowHeight
 
@@ -48,8 +42,6 @@ function draw() {
 
   inBrackets = false;
 
-  // let c = 0;
-  
   for (i = 0; i < charPrint; i++) {
     if (currentText.charAt(i) == "+"){ // end of line
       lineY += 80;
@@ -102,39 +94,6 @@ function draw() {
       pause += 1;
     }
   }
-
-  // while (i < charPrint) {
-  //   let c = line[i];
-  //   if (c == "+"){ // end of line
-  //     lineY += 80;
-  //     lineX = 300;
-  //   } else {
-  //     if (c == "[") {
-  //       inBrackets = true;
-  //     } else if (line[i-1] == "]") {
-  //       inBrackets = false;
-  //       lastChoiceIndex = i;
-  //       pauseForInput = true;
-  //     }
-  //     if (inBrackets)
-  //       fill("deeppink");
-  //     else 
-  //       fill("black");     
-
-  //     text(c, lineX, lineY);
-  //     lineX += (12);
-  //   } 
-
-  //   // newline
-  //   if (lineX > windowWidth - 300 && line[i+1] == " ") {
-  //     i += 1;
-  //     lineY += 40;
-  //     lineX = 300;
-  //   }
-
-  //   // if (!pauseForInput) 
-  //     i += 1;
-  // }
 
   choices[0] = currentText.substring(currentText.indexOf("[") + 1, currentText.indexOf("/")); 
   choices[1] = currentText.substring(currentText.indexOf("/") + 1, currentText.indexOf("]")); 
