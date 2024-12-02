52214b5445618ad1b4f487aba36228b5c2be3233
Ace Chen <12aschen@berkeley.edu>
2024-12-02 02:16:46 -0800

    updated scene 4

--- sketches/choices.js
+++ sketches/choices.js
@@ -18,6 +18,12 @@ let circleSize = 60;
 let startTap = false;
 let ended = false;
 
+let quiz = false;
+let answers = [];
+let correctAnswers = ["ArrowLeft", "ArrowLeft", "ArrowRight", "ArrowRight", "ArrowLeft", "ArrowRight", "ArrowRight"]
+let failure = "";
+let score = 0;
+
 let jetbrains;
 
 function preload() {
@@ -27,7 +33,7 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth - 20, windowHeight - 20);
-  frameRate(48);
+  frameRate(60);
   wHeight = windowHeight
 
   currentText = sceneText[0];
@@ -47,17 +53,29 @@ function draw() {
   inBrackets = false;
 
   for (i = 0; i < charPrint; i++) {
-    if (currentText.charAt(i) == "+"){ // end of line
+    let currentChar = currentText.charAt(i);
+    if (currentChar == "+"){ // end of line
       lineY += 80;
       lineX = 300;
-    } else if (currentText.charAt(i) == "*") {
-      startTap = true;
+    } else if (currentChar == "*") {
+      pauseForInput = true;
+    } else if (currentChar == "&" && quiz == false) {
+      quiz = true;
+      currentText = currentText.substring(0, i) + currentText.substring(i + 1);
+    } else if (currentChar == "^" && quiz == true) {
+      quiz = false;
+      currentText = currentText.substring(0, i) + currentText.substring(i + 1);
+    } else if (currentChar == "%") {
+      scoreCheck();
+    } else if (currentChar == "$") {
+      failCheck();
+      currentText = currentText.substring(0, i) + failure + currentText.substring(i + 1);
     } else {
-      if (currentText.charAt(i) == "[") {
+      if (currentChar == "[") {
         inBrackets = true;
       } else if (currentText.charAt(i-1) == "]") {
         inBrackets = false;
-      } else if (currentText.charAt(i) == "]") {
+      } else if (currentChar == "]") {
         pauseForInput = true;
       }
       if (inBrackets)
@@ -65,18 +83,16 @@ function draw() {
       else 
         fill("black");
       
-      text(currentText.charAt(i), lineX, lineY);
+      text(currentChar, lineX, lineY);
       lineX += (12);
       
       // newline
-      if (lineX > windowWidth - 300 && currentText.charAt(i) == " ") {
+      if (lineX > windowWidth - 300 && currentChar == " ") {
         lineY += 40;
         lineX = 300;
       }
     } 
   }
-
-  print(pauseForInput);
   
   // add new char
     if (i < currentText.length && !ended) {
@@ -92,13 +108,20 @@ function draw() {
   // pause and go to next para
   if (i == currentText.length) {
     if (pause > frameRate() && sceneText[lineNumber + 1] != undefined) { 
-      print(frameRate())
       lineNumber += 1;
-      currentText += "+"
-      currentText += sceneText[lineNumber];
+      if (lineNumber == 4 || lineNumber == 12 || lineNumber == 19 || lineNumber == 25) {
+        currentText = "+";
+        currentText = sceneText[lineNumber];
+        i = 0;
+        charPrint = 1;
+        scrollOffset = 120;
+      } else {
+        currentText += "+"
+        currentText += sceneText[lineNumber];
+      }
       pause = 0;
     } else {
-      pause += 1;
+      pause += 30;
     }
   }
 
@@ -116,7 +139,7 @@ function draw() {
     fill("deeppink");
     if (circleSize == 0) {
       scrollOffset = 120;
-      currentText = "TEACHER: Hey, you! Wake up!";
+      currentText = "TEACHER: Are you listening?+(END OF SCENE 4)";
       charPrint = currentText.length;
       ended = true;
     } else {
@@ -128,16 +151,65 @@ function draw() {
 }
 
 function keyPressed() {
+  if (!startTap && key === " " && pauseForInput) {
+    pauseForInput = false;
+    let newText = currentText.substring(0, currentText.indexOf("*"));
+    newText += currentText.substring(currentText.indexOf("*") + 1);
+    currentText = newText;
+    startTap = true;
+  }
+  if (key === " ") {
+    circleSize = 60;
+  }
   if (pauseForInput && (key === "ArrowLeft" || key === "ArrowRight")) {
     let newText = currentText.substring(0, currentText.indexOf("["));
-    if (key === "ArrowLeft")
+    if (key === "ArrowLeft") {
       newText += choices[0];
-    else if (key === "ArrowRight")
+      charPrint -= choices[1].length + 3;
+      i -= choices[1].length + 3;
+    } else if (key === "ArrowRight") {
       newText += choices[1];
+      charPrint -= choices[0].length + 3;
+      i -= choices[0].length + 3;
+    }
     newText += currentText.substring(currentText.indexOf("]") + 1);
     currentText = newText;
-    charPrint -= choices[1].length + 3;
+    if (quiz) {
+      answers.push(key);
+    }
     pauseForInput = false;
   }
-  circleSize = 60;
+}
+
+function failCheck() {
+  failure = answers.pop();
+  if (failure == "ArrowRight") 
+    failure = "highest";
+  else if (failure == "ArrowLeft")
+    failure = "lowest";
+}
+
+function scoreCheck() {
+  for (let a = 0; a < 7; a++) {
+    if (answers[a] == correctAnswers[a]) 
+      score += 1;
+  }
+  let line = score.toFixed() + " out of 7. ";
+  
+  if (score == 0 && failure == "lowest") {
+    line += "You failed both the quiz and the unit.)"
+  } else if (score == 0 && failure == "highest") {
+    line += "You've failed the quiz, but your grade is hanging on.)"
+  } else if (score == 7 && failure == "highest") {
+    line += "You aced the quiz, but you've failed the unit.)";
+  } else if (score == 7 && failure == "lowest") {
+    line += "You aced the quiz.)";
+  } else  {
+    line += "You're in the clear.)"
+  }
+  
+  let newText = currentText.substring(0, currentText.indexOf("%"));
+  newText += line;
+  newText += currentText.substring(currentText.indexOf("%") + 1);
+  currentText = newText;
 }
